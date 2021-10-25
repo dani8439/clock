bfb11bb5bfc67f99b9fceb7030d49af254a9e67c
Dani <dani8439@aol.com>
2021-10-25 12:21:38 -0400

    created setRotation, setClock functions to manipulate the hands ticking by, and to load in correct position when page loads.

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ function setClock() {
   //   divided by 60 as 60 seconds in a minute
   const secondsRatio = currentDate.getSeconds() / 60;
   //   add in secondsRatio so that the hands move slowly and don't jump every minute or hour, so it moves like a real clock.
-  const minutesRatio = secondsRatio + currentDate.getMinutes() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
   //   divided by 12 as 12 hours on a clock and not 60
-  const hourssRatio = minutesRatio + currentDate.getHours() / 12;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
 }
+
+function setRotation(element, rotationRatio) {
+  //   get style of element, call setProperty, which will take in CSS property we want to set, set it to rotationRatio times 360 as 360 degrees in a circle.
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+//  puts hands in correct position when the page loads
+setClock();
